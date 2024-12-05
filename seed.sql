11c58cb28a2be5333173e0697447abe7d72e6b45
Anas Bashir <anas.bashir@icloud.com>
2024-12-05 19:35:38 +0000

    Populated products table

--- seed.sql
+++ seed.sql
@@ -4,5 +4,15 @@ INSERT INTO Category (name) VALUES
 ('Accessories'),
 ('Coffee Capsules'),
 ('Instant Coffee'),
-('Decaf Coffee'),
-('Cold Brew');
+('Decaf Coffee');
+
+-- Insert into Products
+INSERT INTO Products (name, description, price, category_id, image_url, stock_level, size) VALUES
+('Nicaraguan Bean', 'Origin Coffee Nicaraguan Bean', 10.00, 1, 'N/A', 'in stock', 'Large'),
+('Colombian', 'Colombian Origin Bean', 12.00, 1, 'N/A', 'in stock', 'Large'),
+('Brazilian', 'Brazilian Origin Bean', 11.50, 1, 'N/A', 'low stock', 'Large'),
+('Chocolate', 'Chocolate Coffee Capsule', 8.00, 3, 'N/A', 'in stock', 'Standard'),
+('Vanilla', 'Vanilla Coffee Capsule', 7.50, 3, 'N/A', 'in stock', 'Standard'),
+('Salted Caramel', 'Salted Caramel Instant Coffee', 5.00, 4, 'N/A', 'in stock', 'Small'),
+('Decaf Black', 'Black Decaf Coffee', 6.00, 5, 'N/A', 'in stock', 'Small');
+

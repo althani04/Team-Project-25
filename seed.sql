446b7ecda99a1e12cf9f09f1311056317e6a9eb1
Anas Bashir <anas.bashir@icloud.com>
2024-12-06 09:00:25 +0000

    Populated whole coffee beans category

--- seed.sql
+++ seed.sql
@@ -8,13 +8,13 @@ INSERT INTO Category (name) VALUES
 
 -- Insert into Products
 INSERT INTO Products (name, description, price, category_id, image_url, stock_level, size) VALUES
-('Nicaraguan Bean', 'Origin Coffee Nicaraguan Bean', 10.00, 1, 'N/A', 'in stock', 'Large'),
-('Colombian', 'Colombian Origin Bean', 12.00, 1, 'N/A', 'in stock', 'Large'),
-('Brazilian', 'Brazilian Origin Bean', 11.50, 1, 'N/A', 'low stock', 'Large'),
-('Chocolate', 'Chocolate Coffee Capsule', 8.00, 3, 'N/A', 'in stock', 'Standard'),
-('Vanilla', 'Vanilla Coffee Capsule', 7.50, 3, 'N/A', 'in stock', 'Standard'),
-('Salted Caramel', 'Salted Caramel Instant Coffee', 5.00, 4, 'N/A', 'in stock', 'Small'),
-('Decaf Black', 'Black Decaf Coffee', 6.00, 5, 'N/A', 'in stock', 'Small');
+('Nicaraguan Coffee Beans', 'Single-origin Nicaraguan coffee beans.', 10.00, 1, 'N/A', 'in stock', 'Whole Beans'),
+('Colombian Coffee Beans', 'Single-origin Colombian coffee beans.', 12.00, 1, 'N/A', 'in stock', 'Whole Beans'),
+('Brazilian Coffee Beans', 'Single-origin Brazilian coffee beans.', 11.50, 1, 'N/A', 'in stock', 'Whole Beans'),
+('Ethiopian Coffee Beans', 'Single-origin Ethiopian coffee beans.', 12.00, 1, 'N/A', 'in stock', 'Whole Beans'),
+('Arabic Coffee Beans', 'Single-origin Arabic coffee beans.', 12.00, 1, 'N/A', 'in stock', 'Whole Beans'),
+
+
 
 -- Insert into Users
 INSERT INTO Users (name, email, password, role, phone_number, address_line, postcode) VALUES
